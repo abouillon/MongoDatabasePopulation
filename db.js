bbba01eea74f63667cc4c3aeb6f8ded4adfd02b9
abouillon <a.bouillon21@gmail.com>
2020-09-05 20:08:55 -0400

    Populate Database
    Initial implementation of database population.

--- db.js
+++ db.js
@@ -1,23 +1,87 @@
+const mongo = require("mongodb");
+const dataFormat = require("./schema");
+const movieData = require("./films");
+const mongoclient = new mongo.MongoClient(new mongo.Server("localhost", 27017), {useUnifiedTopology: true});
+processTitles();
+
+function processTitles(){
+    var movies = movieData.data, titles = [], formattedMovies = [];
+    //pull all unqiue titles from data
+    for(var a = 0, iter = movies.length; a < iter; a++){
+        if(!titles.includes(movies[a][8])){
+            titles.push(movies[a][8]);
+        }
+    }
+
+    //grab full data for all movies that match given title
+    for(var b = 0, len = titles.length; b < len; b++){
+        var matchedTitle = [];
+        for(var c = 0, d = movies.length; c < d; c++){
+            if(movies[c].includes(titles[b])){
+                matchedTitle.push(movies[c]);
+            }
+        }
+        var cleanMovie = formatTitle(matchedTitle);
+        matchedTitle = [];
+        formattedMovies.push(cleanMovie);
+    }
+    populatedDB(formattedMovies);
+}
+
 /**
  * Movie title data[i][8]
  * Year data[i][9]
  * Locations data[i][10] (concat then split)
- * Prod Company data[i][12]
- * Distr data[i][13]
- * Director data[i][14]
- * Writer data[i][15]
+ * Prod Company data[i][11]
+ * Distr data[i][12]
+ * Director data[i][13]
+ * Writer data[i][14]
  * Actors data[i][16-18] obj {name: string, locations: string} Grab from locations
  */
-const mongo = require("mongodb");
-const mongoose = require("mongoose");
-const dataFormat = require("./schema");
-const movieData = require("./films");
-var movies = movieData.data, iter = 1;
+function formatTitle(movieTitle){
+    var movie = dataFormat;
+    movie.title = movieTitle[0][8] != null ? movieTitle[0][8] : "";
+    movie.releaseYear = movieTitle[0][9] != null ? movieTitle[0][9] : "";
+    movie.locations = [];
+    movie.productionCompany = movieTitle[0][11] != null ? movieTitle[0][11] : "";
+    movie.distributor = movieTitle[0][12] != null ? movieTitle[0][12] : "";
+    movie.director = movieTitle[0][13] != null ? movieTitle[0][13] : "";
+    movie.writer = movieTitle[0][14] != null ? movieTitle[0][14] : "";
+    movie.actors = [{"name": movieTitle[0][16] != null ? movieTitle[0][16] : "", "locations" : []}, 
+                    {"name": movieTitle[0][17] != null ? movieTitle[0][17] : "", "locations" : []}, 
+                    {"name": movieTitle[0][18] != null ? movieTitle[0][18] : "", "locations" : []}];
 
-while(iter > 0){
-    for(var i = 0, len = 5; i < len; i++){
-        var title = movies[i][8], preTitle = movies[i - 1][8] != undefined ? movies[i - 1][8] : "";
-        
+    for(var i = 0, len = movie.actors.length; i < len; i++){
+        for(var j = 0, locLen = movieTitle.length; j < locLen; j++){
+            if(!movie.locations.includes(movieTitle[j][10])){
+                movie.locations.push(movieTitle[j][10]);
+            }
+            movie.actors[i].locations.push(movieTitle[j][10]);
+        }
     }
-    iter--;
+    
+    return movie;
+}
+
+function populatedDB(sanitizedData){
+    console.log(`Attempting to insert ${sanitizedData.title}`);
+    mongoclient.connect().then((db) => 
+        {
+            console.log("DB Opened");
+            var dbo = db.db("movieDB");
+            dbo.collection("movies").insertMany(sanitizedData, {ordered: false}).then(
+                (res) => {console.log(`${res}`);},
+                (err) => {
+                    console.log(err); 
+                    db.close().then(
+                    (res) => {console.log(`Closed Connection\n\t${res}`);},
+                    (err) => {console.log(err); process.exit()}
+                );}
+            );
+            db.close().then(
+                (res) => {console.log(`Closed Connection\n\t${res}`);},
+                (err) => {console.log(err); process.exit()}
+            );
+        },
+        (err) => {console.log(err.message); process.exit();});
 }
